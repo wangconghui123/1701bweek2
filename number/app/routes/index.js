9f46e766b7efd6062e9b54f9f4b9ea0367abdb00
wch <1114590177@qq.com>
2019-04-22 09:51:54 +0800

    定义借口请求数据

--- number/app/routes/index.js
+++ number/app/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
+router.get('/pai/getOne', function(req, res.next) {
+    res.end("请求数据")
+})
 module.exports = router;
